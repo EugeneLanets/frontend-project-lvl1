93299a8886d176e221353bfa351817a0875e638b
Eugene Lanets <swarrt@yandex.ru>
2021-11-29 20:18:56 +0300

    refactor: make brain-even.js only executable file without logic

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,33 +1,5 @@
 #!/usr/bin/env node
-import { getAnswer, getRandomNumber } from '../src/common.js';
 
-const ROUNDS_TO_WIN = 3;
-
-const evenGameRound = () => {
-  const number = getRandomNumber(1, 99);
-  const isNumberEven = !(number % 2);
-  const rightAnswer = isNumberEven ? 'yes' : 'no';
-
-  console.log('Question:', number);
-  const answer = getAnswer('Your answer: ').toLowerCase();
-
-  return answer === rightAnswer;
-};
-
-const evenGame = () => {
-  let roundNumber = 0;
-  while (ROUNDS_TO_WIN - roundNumber) {
-    const result = evenGameRound();
-    if (!result) {
-      console.log('Incorrect. You lose.');
-      return;
-    }
-
-    console.log('Correct!');
-    roundNumber += 1;
-  }
-
-  console.log();
-};
+import evenGame from '../src/games/brain-even.js';
 
 evenGame();
