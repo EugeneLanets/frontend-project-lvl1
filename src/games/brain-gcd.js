5f39819a940804bddd281c9c6f490e13e377d06a
Eugene Lanets <swarrt@yandex.ru>
2021-11-29 22:25:46 +0300

    feat: add gcd game

---
+++ src/games/brain-gcd.js
@@ -0,0 +1,19 @@
+import runGame from '../index.js';
+import { getRandomNumber } from '../common.js';
+
+const gcd = (a, b) => (b === 0 ? Math.abs(a) : gcd(b, a % b));
+
+const DESCRIPTION = 'Find the greatest common divisor of given numbers';
+
+const getGameData = () => {
+  const first = getRandomNumber(1, 99);
+  const second = getRandomNumber(1, 99);
+
+  const answer = gcd(first, second);
+
+  return [`${first} and ${second}`, String(answer)];
+};
+
+const gcdGame = () => runGame(DESCRIPTION, getGameData);
+
+export default gcdGame;
