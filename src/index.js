c3c878a628aec9da661391d0ec263af61e88a9a1
Eugene Lanets <swarrt@yandex.ru>
2021-11-29 20:16:52 +0300

    refactor: add runGame function to run any game

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import { getAnswer } from './common.js';
+
+const ROUNDS_COUNT = 3;
+
+const runGame = (description, getGameData) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = getAnswer('May I have your name? ');
+  console.log('Hello,', userName);
+
+  console.log(description);
+
+  for (let i = 0; i < ROUNDS_COUNT; i += 1) {
+    const [question, answer] = getGameData();
+
+    console.log('Question:', question);
+
+    const userAnswer = getAnswer('Your answer: ');
+    if (userAnswer !== answer) {
+      console.log(`"${userAnswer}" is wrong answer. Correct answer was "${answer}". You lose!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}! You win!`);
+};
+
+export default runGame;
