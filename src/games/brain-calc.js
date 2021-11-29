9bd1ab0fcbd0e5fa21ca5ba91acc8dda524c909b
Eugene Lanets <swarrt@yandex.ru>
2021-11-29 21:22:11 +0300

    feat: add brain-calc game

---
+++ src/games/brain-calc.js
@@ -0,0 +1,37 @@
+import runGame from '../index.js';
+import { getRandomNumber } from '../common.js';
+
+const sum = (a, b) => a + b;
+const diff = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+
+const OPERATIONS = [
+  {
+    func: sum,
+    sign: '+',
+  },
+  {
+    func: diff,
+    sign: '-',
+  },
+  {
+    func: multiply,
+    sign: '*',
+  },
+];
+
+const DESCRIPTIONS = 'What is the result of the expression?';
+
+const getGameData = () => {
+  const first = getRandomNumber(1, 99);
+  const second = getRandomNumber(1, 99);
+  const operation = OPERATIONS[getRandomNumber(0, OPERATIONS.length)];
+
+  const answer = operation.func(first, second);
+
+  return [`${first} ${operation.sign} ${second}`, String(answer)];
+};
+
+const calcGame = () => runGame(DESCRIPTIONS, getGameData);
+
+export default calcGame;
